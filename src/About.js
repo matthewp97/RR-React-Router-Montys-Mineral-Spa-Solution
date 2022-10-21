ca4b3f936806269a3a2d53ffd4408d9c3434967a
Matthew Perrot <Matthew@Matthews-MacBook-Air.local>
2022-10-20 18:21:43 -0700

    added bootstrap

---
+++ src/About.js
@@ -0,0 +1,23 @@
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+
+<Container>
+    <Card border="info" >
+        <Card.Header className="blockquote mb-0 card-body">About Us</Card.Header>
+        <Card.Body>
+            <Card.Text>
+                When returning from a spritual journey through the Earth, ....
+            </Card.Text>
+            <Card.Text>
+                As the heir of the industrious McMansions fortune, Monty ....
+            </Card.Text>
+            <Card.Text>
+                Monty decided to travel around the ...
+            </Card.Text>
+            <Card.Text>
+                Upon his return, he dedicated about 1% ...
+            </Card.Text>
+        </Card.Body>
+        <Card.Img style={{ width: '50%', margin: '0 auto'}}  variant="bottom" src="hotsprings.jpg" />
+    </Card>
+</Container>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+<Container>
+    <Nav defaultActiveKey="/" variant="tabs" fill>
+        <Nav.Item>
+            <Nav.Link href="/"> 
+                <Link to="/">Home</Link>
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item >
+            <Nav.Link eventKey={"aboutPage"}> 
+                <Link to="/about">About Us</Link>
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item >
+            <Nav.Link eventKey={"packagesPage"}> 
+                <Link to="/packages">Our Packages</Link> 
+            </Nav.Link>
+         </Nav.Item>
+    </Nav>
+</Container>
 
 ReactDOM.render(
   <React.StrictMode>
